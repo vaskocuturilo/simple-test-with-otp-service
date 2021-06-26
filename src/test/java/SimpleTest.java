3e94b791e27c121b85e5b6d40324049a14b087d5
asm@dostavista.ru <wolfstonik>
2021-06-26 11:11:56 +0300

    QA-0001: Add simple an automation script with one time password, without pattern.

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,52 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class SimpleTest {
+    WebDriver webDriver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void testSimpleUsingOneTimePassword() {
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.navigate().to("https://test-engineer.site/test-room/");
+        Assertions.assertEquals(webDriver.getTitle(), "Login Page");
+
+        //Email field
+        webDriver.findElement(By.id("username-field")).clear();
+        webDriver.findElement(By.id("username-field")).sendKeys("user");
+
+        //Password field
+        webDriver.findElement(By.id("password-field")).clear();
+        webDriver.findElement(By.id("password-field")).sendKeys("user");
+
+        //One time password field
+        webDriver.findElement(By.id("otp-field")).clear();
+        webDriver.findElement(By.id("otp-field")).sendKeys("1234");
+
+        //Submit button
+        webDriver.findElement(By.id("login-form-submit")).click();
+
+        Assertions.assertEquals(webDriver.getTitle(), "Profile Page");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        webDriver.manage().deleteAllCookies();
+        webDriver.quit();
+    }
+}
